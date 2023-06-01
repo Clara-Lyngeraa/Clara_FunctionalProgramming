f8b4ae9a88a58abd876055d0c5ae414a3ede12a7
Clara-Lyngeraa <111750232+Clara-Lyngeraa@users.noreply.github.com>
2023-06-01 13:07:22 +0200

    Add files via upload

---
+++ Assignment4.2/Dictionary/Library.fs
@@ -0,0 +1,11 @@
+﻿module Dictionary
+
+type Dictionary = Dict of List<string>
+
+let empty (u: unit) = Dict List.empty
+
+let insert s (Dict(dict)) = Dict(s :: dict)
+let lookup s (Dict(dict)) = List.contains(s) dict
+
+printfn "%A"  (empty ())
+printfn "%A" (lookup "HELLO" (empty () |> insert "HELLO"))
---
+++ Assignment4.2/Dictionary/Library.fsi
@@ -0,0 +1,7 @@
+module Dictionary
+
+type Dictionary
+
+val empty : unit -> Dictionary
+val insert : string -> Dictionary -> Dictionary
+val lookup : string -> Dictionary -> bool 
